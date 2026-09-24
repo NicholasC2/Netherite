e38937a2e2ad2147ccbe320079a5d33a01d3b8a2
NicholasC2 <nicholasc2@proton.me>
2026-09-25 11:20:34 +1200

    doesnt build

--- src/Netherite.Client/Program.cs
+++ src/Netherite.Client/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using SDL3;
 
-class Program
+internal class Program
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        if (!SDL.Init(SDL.InitFlags.Video))
+        {
+            Console.WriteLine("[SDL] Init failed: " + SDL.GetError());
+            return;
+        }
+
+        Console.WriteLine("[SDL] Initialized successfully");
+
+        nint window = SDL.CreateWindow(
+            "Netherite.Client",
+            640,
+            480,
+            SDL.WindowFlags.OpenGL
+        );
+
+        if (window == nint.Zero)
+        {
+            Console.WriteLine("[SDL] Window creation failed: " + SDL.GetError());
+            SDL.Quit();
+            return;
+        }
+
+        bool running = true;
+
+        while(running) {
+            while(SDL.PollEvent(out SDL.Event e)) {
+                if (e.Type == SDL) {
+                    running = false;
+                }
+            }
+        }
+
+        SDL.DestroyWindow(window);
+        SDL.Quit();
     }
 }
